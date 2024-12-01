d99ab80469471d16f8404c7f132699d48e42a075
javaing63 <arttseng@gmail.com>
2024-12-01 09:26:30 +0800

    first commit

--- app/src/main/java/com/arttseng/homeexamtravel/tools/Const.kt
+++
@@ -1,9 +0,0 @@
-package com.arttseng.homeexamtravel.tools
-
-class Const {
-    companion object {
-        const val BaseURL = "https://www.travel.taipei/open-api/"
-        const val GetAttractionsAll = "{lang}/Attractions/All"
-        const val GetEventNews = "{lang}/Events/News"
-    }
-}
